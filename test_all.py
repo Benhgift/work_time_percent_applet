dfb0b6e39fc95c725da4515532adb4b960c88cf4
benhgift <benhgift@gmail.com>
2017-03-19 17:32:02 -0700

    fixed issue #7, bug when range crosses midnight

--- test_all.py
+++ test_all.py
@@ -5,14 +5,16 @@ from datetime import datetime
 # If your work day is between 9am and 5pm, make sure that at 1pm it displays 50%
 @freeze_time("13:00:00")
 def test_basic_percent_calculation():
-    workPercent = work_percent.WorkPercent(9, 17)
-    percent = workPercent.work_percent()
-    assert percent == 50
+    _test_half_way_between_range(9, 17)
 
 
-# Now if your work day starts late at night (9pm), make sure it can cross midnight if you work till 3am
-@freeze_time("00:00:00")
-def test_basic_percent_calculation():
-    workPercent = work_percent.WorkPercent(21, 3)
+# Now if your work day starts late at night (10pm), make sure it can cross midnight if you work till 4am
+@freeze_time("01:00:00")
+def test_percent_calculation_when_range_crosses_midnight():
+    _test_half_way_between_range(22, 4)
+
+
+def _test_half_way_between_range(start, end):
+    workPercent = work_percent.WorkPercent(start, end)
     percent = workPercent.work_percent()
     assert percent == 50
--- work_percent.py
+++ work_percent.py
@@ -6,12 +6,33 @@ class WorkPercent:
     def __init__(self, work_start, work_end):
         self.work_start = 60 * 60 * work_start
         self.work_end = 60 * 60 * work_end
-        self.seconds_in_work_day = self.work_end - self.work_start
+        self.seconds_in_day = 60 * 60 * 24
+        self.special_midnight_crossing_case = False
+        if self.work_start > self.work_end:
+            self.special_midnight_crossing_case = True
+            self.seconds_in_work_day = self.seconds_in_day - self.work_start
+            self.seconds_in_work_day += self.work_end
+        else:
+            self.seconds_in_work_day = self.work_end - self.work_start
+
+    def _special_midnight_wrapping_percent(self, current_seconds):
+        if current_seconds >= self.work_start or current_seconds < self.work_end:
+            if current_seconds >= self.work_start: 
+                total = current_seconds - self.work_start
+            else:
+                total = self.seconds_in_day - self.work_start
+                total += current_seconds
+            return (total/self.seconds_in_work_day) * 100
+        else:
+            return 0
 
     def work_percent(self):
         now = datetime.now()
         start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
         seconds = (now - start_of_day).seconds
+        # if it's the special midnight crossing case
+        if self.special_midnight_crossing_case:
+            return self._special_midnight_wrapping_percent(seconds)
         if seconds <= self.work_start:
             return 0
         elif seconds >= self.work_end:
